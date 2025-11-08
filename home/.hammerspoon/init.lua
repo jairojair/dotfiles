79f1448600f65228d0939466a74e93f6f679121c
jairojair <jairojair@gmail.com>
2025-11-07 21:18:16 -0500

    Add hotkey bindings

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,1 +1,30 @@
-hs.alert.show("Hello, world!")
+hs.alert.show("Reloaded")
+
+hs.hotkey.bind({"alt"}, "R", function()
+    hs.reload()
+end)
+
+local function openApp(name)
+    local app = hs.application.get(name)
+    
+    hs.application.launchOrFocus(name)
+
+    if app:allWindows() == 0 then
+        app:activate()
+        app.newWindow()
+    end
+end
+
+local function openSafari()
+    openApp("Safari")
+end
+local function openChrome()
+    openApp("Google Chrome")
+end
+local function openTerminal()
+    openApp("Ghostty")
+end
+
+hs.hotkey.bind({"alt"}, "S", openSafari)
+hs.hotkey.bind({"alt"}, "C", openChrome)
+hs.hotkey.bind({"alt"}, "T", openTerminal)
